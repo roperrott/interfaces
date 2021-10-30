9a8b639bab2c1a1d8e671adf9b8654310aa15e85
=Marcos Acosta Torrissi <marcos.acosta.em@gmail.com>
2021-10-29 21:50:44 -0300

    massive changes

--- Entregable3/js/game.js
+++ Entregable3/js/game.js
@@ -1,81 +1,103 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let gameOver = document.querySelector('.game-over');
+    let divGameOver = document.querySelector('.game-over');
     let ground = document.querySelector('.ground');
     let container = document.querySelector('.container');
     let score = document.querySelector('.score');
-    let scoreValue = document.querySelector('#score-value')
+    let scoreValue = document.querySelector('#score-value');
     let ufo = document.querySelector('.layer-4');
     let cloud = document.querySelector('.layer-3');
     let cactus = document.querySelector('.layer-2');
     let tRex = document.querySelector('.t-rex');
-    let testCactus = document.querySelector('.layer-6');
-    let testUfo = document.querySelector('.layer-7');
-    let isEnd = false;
+    let octopus = document.querySelector('.layer-6');
+    let isEnd = true;
     let keyDown = false;
     let keyCodes = 0;
     let isFlying = false;
     let isJumping = false;
+    let intervalId;
 
     window.addEventListener('keydown', (e) =>{
+        if (isEnd && e.code == 'Enter') {
+            console.log("START GAME BECAUSE KEYDOWN");
+            startGame();
+        }
         keyDown = true;
         keyCodes = e.code;
     });
 
-    // window.addEventListener('keyup', (e) => {
-    //     keyDown = false; 
-    // })
-
-    let intervalId = setInterval(function()
-    {
-        calculateScore();
-        checkColission();
-        if(keyDown && !isFlying){
-            if(keyCodes === 'Space'){
-                isJumping = true;
-                jump(); 
-            }else if( keyCodes === 'KeyS' && isJumping){
-                isFlying = true;
-                superMode();
-            }else if( keyCodes === 'ArrowRight'){
-                runFaster();
-            }
-        }else{
-
-        }
-    }, 70 /*ajustar al tiempo necesario*/);
-
-    if(isEnd){
-        window.clearInterval(intervalId);
-    }
-
     function checkColission() {
-    
+        const ufoOffsetY = -250;
+
         // T-rex 
-        let heroWidth = tRex.getBoundingClientRect().width
         let heroHeight = tRex.getBoundingClientRect().height
 
         // Alien bug position
-        let cactusX = testCactus.getBoundingClientRect().x;
-        let cactusY = testCactus.getBoundingClientRect().y;
+        let octopusX = octopus.getBoundingClientRect().x;
+        let octopusY = octopus.getBoundingClientRect().y;
 
         // Hero bounds
-        // ESTAN MAL CALCULADAS ACTUALMENTE
-        let fromX = tRex.getBoundingClientRect().x - 42;
-        let toX = tRex.getBoundingClientRect().x + 42;
+        let fromX = tRex.getBoundingClientRect().x - 20;
+        let toX = tRex.getBoundingClientRect().x + 20;
         let fromY = tRex.getBoundingClientRect().y - (heroHeight / 2);
         let toY = tRex.getBoundingClientRect().y + (heroHeight / 2);
 
-        let ufoX = testUfo.getBoundingClientRect().x;
-        let ufoY = testCactus.getBoundingClientRect().y;
+        let ufoX = ufo.getBoundingClientRect().x;
+        let ufoY = octopus.getBoundingClientRect().y + ufoOffsetY;
 
-        if ((cactusX > fromX && cactusX < toX) && (cactusY > fromY && cactusY < toY)) {
-            // GAME OVER
-            console.log("MUERTE EN TIERRA")
+        if (keyCodes != 'KeyS') {
+            if ((octopusX > fromX && octopusX < toX) && (octopusY > fromY && octopusY < toY)) {
+                console.log("MUERTE EN TIERRA")
+                gameOver();
+            }
+            if ((ufoX > fromX && ufoX < toX) && (ufoY > fromY && ufoY < toY)) {
+                console.log("MUERTE EN Aire")
+                gameOver();
+            }
         }
-        if ((ufoX > fromX && ufoX < toX) && (ufoY > fromY && ufoY < toY)) {
-            // GAME OVER
-            console.log("MUERTE EN AIRE")
-        }
+    }
+
+    function startInterval() {   
+        intervalId = setInterval(function()
+        {
+            calculateScore();
+            checkColission();
+            if(keyDown && !isFlying){
+                if(keyCodes === 'Space'){
+                    isJumping = true;
+                    jump(); 
+                }else if( keyCodes === 'KeyS' && isJumping){
+                    isFlying = true;
+                    superMode();
+                }
+            }
+        }, 70);
+    }
+
+    function resetScore() {
+        scoreValue.innerHTML = 0;
+    }
+
+    function startGame() {
+        isEnd = false;
+        resetScore();
+        startInterval();        
+        ground.classList.add('ground-move');
+        cactus.classList.add('cactus-move');
+        cloud.classList.add('cloud-move');
+        tRex.classList.add('running');
+        octopus.classList.add('octopus-move');
+        ufo.classList.add('ufo-move');
+    }
+    
+    function gameOver() {
+        window.clearInterval(intervalId);
+        tRex.classList.remove('running');
+        ground.classList.remove('ground-move');
+        cactus.classList.remove('cactus-move');
+        cloud.classList.remove('cloud-move');
+        octopus.classList.remove('octopus-move');
+        ufo.classList.remove('ufo-move');
+        isEnd = true;
     }
 
     function calculateScore() {
@@ -85,15 +107,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function jump(){
-        tRex.classList.remove('running');
-        tRex.classList.add('jumping');
-
-        window.addEventListener('keyup', (e) => {
-            keyDown = false; 
-            tRex.classList.remove('jumping');
-            tRex.classList.add('running');
-            isJumping = false;
-        })
+        if (!isEnd) {
+            tRex.classList.remove('running');
+            tRex.classList.add('jumping');
+        
+            window.addEventListener('keyup', (e) => {
+                keyDown = false; 
+                tRex.classList.remove('jumping');
+                tRex.classList.add('running');
+                isJumping = false;
+            })
+        }
     }
 
     function superMode(){
@@ -104,20 +128,18 @@ document.addEventListener('DOMContentLoaded', () => {
         cactus.classList.add('cactus-super');
         cloud.classList.add('cloud-super');
         ufo.classList.add('ufo-super');
+        octopus.classList.add('octopus-super');
         setTimeout(() => {
             tRex.classList.remove('superheroe');
             ground.classList.remove('ground-super');
             cactus.classList.remove('cactus-super');
             cloud.classList.remove('cloud-super');
             ufo.classList.remove('ufo-super');
-            container.classList.remove('transition-super')
-            
+            container.classList.remove('transition-super');
+            octopus.classList.remove('octopus-super');
+
             keyCodes = "";
             isFlying = false;
         }, 3000);
-    }
-
-    function runFaster(){
-   
     }
 })
